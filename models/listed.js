63e6852bb81375a8d15900994679d8254bdf603f
jaxiroke <jacksiroke@gmail.com>
2023-08-23 14:24:30 +0300

    improved the songs and books routes

--- models/listed.js
+++ models/listed.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const modelSchema = new mongoose.Schema({
-    listedid: { type: Number, unique: true },
+    listedId: { type: Number, unique: true },
     song: { type: Number, required: true },
     title: { type: String, required: false },
     description: { type: String, required: false },
     position: { type: Number, default: 0 },
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date }
+    created: { type: Date, default: Date.now },
+    updated: { type: Date }
 });
 
 module.exports = mongoose.model('Listed', modelSchema);;
